4c5e4edd3f19f0ce9cf39f051a0b617ff00bca26
Ville Pietarinen <ville.pietarinen@gmail.com>
2024-02-22 10:41:06 +0200

    Continue section 13

---
+++ section-13/DefaultConstructor/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+class Player {
+private:
+    std::string name;
+    int health;
+    int xp;
+
+public:
+    Player() {
+        name = "None";
+        health = 100;
+        xp = 3;
+    }
+
+    Player(std::string name_val, int health_val, int xp_val) {
+        name = name_val;
+        health = health_val;
+        xp = xp_val;
+    }
+
+    void set_name(std::string name_val) {
+        name = name_val;
+    }
+
+    std::string get_name() {
+        return name;
+    }
+};
+
+int main() {
+    Player hero;
+    Player frank{"Frank", 100, 13};
+    frank.set_name("Frank");
+    cout << frank.get_name() << endl;
+
+    return 0;
+}
---
+++ section-13/DefaultConstructorParameters/main.cpp
@@ -0,0 +1,25 @@
+#include <string>
+
+using namespace std;
+
+class Player {
+private:
+    std::string name;
+    int health;
+    int xp;
+
+public:
+    Player(std::string name_val = "None", int health_val = 0, int xp_val = 0);
+};
+
+Player::Player(std::string name_val, int health_val, int xp_val)
+    : name{name_val}, health{health_val}, xp{xp_val} {
+}
+
+int main() {
+    Player empty;
+    Player frank{"Frank", 100};
+    Player villain{"Villain", 100, 55};
+
+    return 0;
+}
