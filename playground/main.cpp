2824d7a703305c74b23486db7516727e33a20b0d
Ville Pietarinen <ville.pietarinen@gmail.com>
2024-03-22 10:31:35 +0200

    Begin section 16

--- playground/main.cpp
+++ playground/main.cpp
@@ -1,23 +1,7 @@
 #include <iostream>
-#include <string>
-
-std::string reverse_string(const std::string &str) {
-    std::string reversed;
-    const char* start = str.c_str();
-    const char* end = str.c_str() + str.size() - 1;
-
-    while (end >= start) {
-        reversed.push_back(*end);
-        end--;
-    }
-
-    return reversed;
-}
 
 int main() {
-    std::string input = "Hello, World!";
-    std::string reversed = reverse_string(input);
-    std::cout << reversed << std::endl;
+    std::string foo{"foo"};
 
     return 0;
 }
---
+++ section-16/UsingOverride/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+class Base {
+public:
+  virtual void say_hello() const {
+    std::cout << "Hello - I'm a Base class object" << std::endl;
+  }
+
+  virtual ~Base() {
+  };
+};
+
+class Derived : public Base {
+public:
+  virtual void say_hello() const override {
+    std::cout << "Hello - I'm a Derived class object" << std::endl;
+  }
+
+  virtual ~Derived() {
+  }
+};
+
+int main() {
+  Base* p1 = new Base();
+  p1->say_hello();
+
+  Derived* p2 = new Derived();
+  p2->say_hello();
+
+  Base* p3 = new Derived();
+  p3->say_hello();
+
+  delete p1;
+  delete p2;
+  delete p3;
+
+  return 0;
+}
